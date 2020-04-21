372790857fc04f5f2889cce1f8c82cfe6f1208a5
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-04-21 10:13:41 -0500

    changes to bypass mongo, now going to add to heroku

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,22 +1,22 @@
 # Dependencies
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+#mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
     # Find one record of data from the mongo database
-    mars_info = mongo.db.collection.find_one()
-
+   # mars_info = mongo.db.collection.find_one()
+    mars_info=scrape_mars.scrape()
     # Return template and data
     return render_template("index.html", mars=mars_info)
 
@@ -29,7 +29,7 @@ def scrape():
     mars_dictionary = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_dictionary, upsert=True)
+  #  mongo.db.collection.update({}, mars_dictionary, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
